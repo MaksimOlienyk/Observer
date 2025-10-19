cb5ca744cda5cc7b5736b83c7efe8691b2f1df17
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 19:06:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿interface IObserver { void Update(string msg); }
+class Subject
+{
+    List<IObserver> obs = new();
+    public void Attach(IObserver o) => obs.Add(o);
+    public void Notify(string m) { foreach (var o in obs) o.Update(m); }
+}
+
+class ConcreteObserver : IObserver
+{
+    public void Update(string msg) => Console.WriteLine("Received: " + msg);
+}
+
+class Program
+{
+    static void Main()
+    {
+        var s = new Subject();
+        s.Attach(new ConcreteObserver());
+        s.Notify("Event happened");
+    }
+}
+
